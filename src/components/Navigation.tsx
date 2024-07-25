ff0c67b343e29d0351f77b7a0c0260c641dd2bcb
Nick Mertens <nick@thecrypt.co.nz>
2024-07-25 21:42:41 +1200

    Add logo and hide nav on mobile

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,15 +1,26 @@
+import Image from "next/image";
 import ResponsiveContainer from "./ResponsiveContainer";
 
 export default function Navigation() {
   return (
-    <div className="w-full bg-background/20 fixed top-0">
+    <div className="w-full bg-background/20 absolute md:fixed top-0">
       <ResponsiveContainer>
-        <div className="flex flex-row justify-between ">
-          <div>blod [sic]</div>
-          <nav className="flex flex-row gap-4">
+        <div className="flex flex-row items-center justify-center md:justify-between">
+          <a href="/">
+            <h1 className="flex flex-row gap-4 items-center">
+              <Image
+                src="/images/blod-sic-logo-white.svg"
+                alt=""
+                width={20}
+                height={20 * 1.5}
+              />
+              blod [sic]
+            </h1>
+          </a>
+          <nav className="hidden md:flex flex-row gap-4">
             <a href="#wight">wight</a>
-            <a href="#musicvideos">music videos</a>
             <a href="#discography">discography</a>
+            <a href="#musicvideos">music videos</a>
             <a href="#contact">contact</a>
           </nav>
         </div>
