d6429524bf51cea2d32498ca9185f711da8fb8da
Nick Mertens <nick@thecrypt.co.nz>
2024-07-26 09:31:06 +1200

    Add favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,15 @@
 import type { Metadata } from "next";
 import { Jost } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const jost = Jost({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "blod [sic] — dark lo-fi, horrorwave, ghostwave, dark electronica — auckland, new zealand",
-  description: "Electronic artist 'blod [sic]' based out of Auckland, New Zealand — explore their singles, EPs, albums and music videos.",
+  title:
+    "blod [sic] — dark lo-fi, horrorwave, ghostwave, dark electronica — auckland, new zealand",
+  description:
+    "Electronic artist 'blod [sic]' based out of Auckland, New Zealand — explore their singles, EPs, albums and music videos.",
 };
 
 export default function RootLayout({
@@ -16,7 +19,32 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${jost.className} bg-background text-foreground`}>{children}</body>
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ab0000" />
+        <meta name="msapplication-TileColor" content="#2d89ef" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
+      <body className={`${jost.className} bg-background text-foreground`}>
+        {children}
+      </body>
     </html>
   );
 }
