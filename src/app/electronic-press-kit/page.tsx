833a2b648bf617a5d9c066c0de871638def05546
Nick Mertens <nick@thecrypt.co.nz>
2024-07-26 17:22:12 +1200

    Update meta title and description for EPK page

--- src/app/electronic-press-kit/page.tsx
+++ src/app/electronic-press-kit/page.tsx
@@ -1,3 +1,4 @@
+import { Metadata } from "next";
 import ContactSection from "@/components/ContactSection";
 import DiscographySection from "@/components/DiscographySection";
 import Divider from "@/components/Divider";
@@ -5,6 +6,12 @@ import MusicVideosSection from "@/components/MusicVideosSection";
 import Navigation from "@/components/Navigation";
 import UpcomingSection from "@/components/UpcomingSection";
 import AboutBlodSicSection from "@/components/electronic-press-kit/AboutBlodSicSection";
+
+export const metadata: Metadata = {
+  title: "blod [sic] — electronic press kit",
+  description:
+    "Electronic press kit for electronic artist 'blod [sic]' based out of Auckland, New Zealand",
+};
 
 export default function Home() {
   return (
