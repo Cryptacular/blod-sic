9384bbd530fe2531da397ec93c96ce303b87c0bc
Nick Mertens <nick@thecrypt.co.nz>
2025-02-07 19:59:20 +1300

    Add divider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,6 +16,7 @@ export default function Home() {
       <HeroSection />
       <DivisionOutNowSection />
       <SpotifyPlayerSection />
+      <Divider />
       <DiscographySection />
       <Divider />
       <MusicVideosSection />
