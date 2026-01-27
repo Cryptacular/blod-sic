687c172787c83b9023c1533152df31ff4d201f33
Nick Mertens <nick@thecrypt.co.nz>
2026-01-28 12:17:19 +1300

    Highlight Phlebotomy I and II

--- src/components/UpcomingSection.tsx
+++ src/components/UpcomingSection.tsx
@@ -8,39 +8,68 @@ export default function UpcomingSection() {
       <ResponsiveContainer>
         <div className="flex flex-col gap-16">
           <h2 className="text-4xl text-center font-bold">
-            <a id="the-space-between" />
-            new album ‘the space between’ out now!
+            <a id="phlebotomy" />
+            new albums ‘phlebotomy I’ and ‘phlebotomy II’ out now!
           </h2>
 
           <div className="flex flex-col md:flex-row gap-16 items-center justify-around">
-            <div className="flex flex-col gap-6 max-w-96 text-xl">
+            <div className="flex flex-col gap-6 max-w-2xl text-xl">
               <p>
-                The Space Between by <em>blod [sic]</em> explores the vast empty
-                space, the silence in the void, the sounds in that silence. Let
-                these five instrumental tracks take you through these liminal,
-                haunting spaces and soak in the haunting soundscape while you
-                sink deeper into your subconscious.
+                Phlebotomy I and Phlebotomy II by <em>blod [sic]</em> are
+                collections of recordings from various jam sessions in 2024 and
+                2025.
               </p>
 
-              <p className="font-bold">Album available now!</p>
+              <p>
+                The tracks have been mildly mixed and mastered for your
+                listening pleasure, but are otherwise the same as when they were
+                recorded. Some of these jam sessions are also on my YouTube
+                channel if you are interested!
+              </p>
+
+              <p>
+                All tracks on these albums are under the Creative Commons
+                license, meaning you are free to use them however you see fit
+                after purchase with attribution. If you do end up using them in
+                something, please send me a message as I’d love to check it out!
+              </p>
             </div>
 
-            <Image
-              src="/images/albums/the-space-between.png"
-              alt="the space between artwork"
-              width={272}
-              height={272}
-              className="border-solid border-16 border-background"
-            />
+            <div className="flex flex-col gap-4">
+              <a
+                href="https://blodsic.bandcamp.com/album/phlebotomy-i"
+                target="_blank"
+              >
+                <Image
+                  src="/images/albums/phlebotomy-I.jpg"
+                  alt="phlebotomy I artwork"
+                  width={272}
+                  height={272}
+                  className="border-solid border-16 border-background"
+                />
+              </a>
+              <a
+                href="https://blodsic.bandcamp.com/album/phlebotomy-ii"
+                target="_blank"
+              >
+                <Image
+                  src="/images/albums/phlebotomy-II.jpg"
+                  alt="phlebotomy II artwork"
+                  width={272}
+                  height={272}
+                  className="border-solid border-16 border-background"
+                />
+              </a>
+            </div>
           </div>
 
           <div className="flex flex-col md:flex-row items-center justify-center gap-8">
             <Button
               variant="dark"
               target="_blank"
-              href="https://blodsic.bandcamp.com/album/the-space-between"
+              href="https://blodsic.bandcamp.com"
             >
-              Check out The Space Between
+              Listen on Bandcamp
             </Button>
           </div>
         </div>
