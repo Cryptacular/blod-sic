19b7ba49f99eefa6cfb4b421de6ed76b8eda5009
Nick Mertens <nick@thecrypt.co.nz>
2026-02-22 20:48:37 +1300

    Update embedded player with Raven

--- src/components/BandcampEmbeddedPlayer.tsx
+++ src/components/BandcampEmbeddedPlayer.tsx
@@ -4,7 +4,7 @@ export default function BandcampEmbeddedPlayer() {
   return (
     <iframe
       style={{ border: 0, width: 500, height: 241 }}
-      src="https://bandcamp.com/EmbeddedPlayer/album=957769023/size=large/bgcol=333333/linkcol=e32c14/artwork=small/transparent=true/"
+      src="https://bandcamp.com/EmbeddedPlayer/album=1197251152/size=large/bgcol=333333/linkcol=e32c14/tracklist=false/artwork=small/transparent=true/"
       seamless
       className="max-w-full"
     />
